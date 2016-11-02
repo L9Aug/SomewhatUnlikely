fde96a13864997b9e70825d13a23736df1093e4f
Ross Sweetland <UP723163@uni.ds.port.ac.uk>
2016-11-02 16:46:35 +0000

    pause menu, reload to intial spawn

---
+++ AnyMeansNecessary/Assets/Scripts/UI/CheckpointScript.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckpointScript : MonoBehaviour {
+
+    public bool isActivated;
+    public static GameObject[] checkPointList;
+    public static GameObject[] EnemyList;
+
+    // Use this for initialization
+    void Start () {
+        checkPointList = GameObject.FindGameObjectsWithTag("CheckPoint"); //Finds all the checkpoints in the level by searching for the tag	
+        EnemyList = GameObject.FindGameObjectsWithTag("Enemy"); //Finds all the enemies in the scene, tag all enmeies with this tag so that there position can also be reset to the poin of checkpoint being activated
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    private void ActivateCheckpoint()
+    {
+        foreach (GameObject checkpoint in checkPointList)
+        {
+            checkpoint.GetComponent<CheckpointScript>().isActivated = false;
+           
+        }
+        isActivated = true;
+        
+    }
+
+    public static Vector3 GetCurrentPositions()
+    {
+        Vector3 posValue = new Vector3(0.0f, 0.418f, 0.0f);
+        if (checkPointList != null)
+        {
+            foreach (GameObject checkpoint in checkPointList)
+            {
+               if(checkpoint.GetComponent<CheckpointScript>().isActivated == true)
+                {
+                    posValue = checkpoint.transform.position;
+                    break;
+                }
+            }
+        }
+
+        return posValue;
+    }
+
+    void OnTriggerEnter(Collider checkpointCollider)
+    {
+        if(checkpointCollider.tag == "Player")
+        {
+            ActivateCheckpoint();
+        }
+    }
+}
---
+++ AnyMeansNecessary/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Button Resume;
+    public Button Reload;
+    public Button Quit;
+
+
+   
+
+
+    // Use this for initialization
+    void Start () {
+
+        disableButtons();
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            enableButtons();
+            Time.timeScale = 0.0f;
+            
+        }
+
+       
+
+
+    }
+
+
+    void disableButtons()// disables pause menu
+    {
+        Resume.gameObject.SetActive(false);
+        Reload.gameObject.SetActive(false);
+        Quit.gameObject.SetActive(false);
+    }
+
+    void enableButtons()//Function brings the pause menu up
+    {
+        Resume.gameObject.SetActive(true);
+        Reload.gameObject.SetActive(true);
+        Quit.gameObject.SetActive(true);
+    }
+
+     void resume() //resumes game
+    {
+        disableButtons();
+        Time.timeScale = 1.0f;
+    }
+
+     void quit() //quits game
+    {
+        Application.Quit();
+        Debug.Log("Is Quitting");
+    }
+
+    void reloadCheckpoint()
+    {
+       GameObject playerPos = GameObject.FindGameObjectWithTag("Player");
+
+        playerPos.transform.position = CheckpointScript.GetCurrentPositions();
+
+        resume();
+        
+        
+    }
+
+   
+}
