5946ac4785bb4112c2a962407bf4c7e58de376eb
Arby27 <up723163@port.ac.uk>
2016-11-14 11:19:40 +0000

    Detection meters above each individual enemy

---
+++ AnyMeansNecessary/Assets/Scripts/UI/DetectionMeter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DetectionMeter : MonoBehaviour {
+
+    public Image meter;
+    public Camera PlayerCam;
+   
+
+  public FieldOfView EnemyFOVScript;
+    
+  float timer;
+	// Use this for initialization
+	void Start () {
+       
+        meter.type = Image.Type.Filled;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        transform.LookAt(transform.position + PlayerCam.transform.rotation * Vector3.forward, PlayerCam.transform.rotation * Vector3.up); // billboards detection meter to always be facing the camera
+        DetetionTimer();
+
+    }
+
+    void DetetionTimer()
+    {
+
+        timer = EnemyFOVScript.detectedtimer;
+        meter.fillAmount = timer/FieldOfView.detectionTimer ;
+
+
+    }
+}
